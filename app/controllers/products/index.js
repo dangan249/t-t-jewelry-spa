0621267521fc50f56b71523ab08cf49497c2795d
An Dang <dang.an249@gmail.com>
2014-11-30 14:02:16 -0500

    filter by categoty

--- app/controllers/products/index.js
+++ app/controllers/products/index.js
@@ -10,15 +10,26 @@ export default Ember.ArrayController.extend({
   category: null,
   unFilteredProducts: Ember.computed.alias('model'),
 
+  // TODO: this somehow cause 2 request when loaded
   products: function() {
-    var query, unFilteredProducts, result;
+    var query, unFilteredProducts, result, brand, gender, category;
 
     query = this.get('query');
+    brand = this.get('brand');
+    gender = this.get('gender');
+    category = this.get('category');
+
     unFilteredProducts = this.get('unFilteredProducts');
 
-    result = query ? this.store.find('product', {query: query}) : unFilteredProducts;
+    result = (query || brand || gender || category) ?
+      this.store.find('product', {
+        query: query,
+        brand: brand,
+        category: category,
+        gender: gender
+      }) : unFilteredProducts;
     return result;
-  }.property('unFilteredProducts','query'),
+  }.property('unFilteredProducts','query', 'category', 'brand', 'genger'),
 
   actions: {
     search: function (searchQuery) {
